cfb38ec964932cab97496383d43e22f5dac66b97
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-12-04 08:49:38 +0000

    feat(day04): solve part 2

--- day04/index.ts
+++ day04/index.ts
@@ -10,7 +10,7 @@ const lines = await readFile(path);
 
 const grid: string[][] = lines.map((line) => line.split(""));
 
-const directions: [number, number][] = [
+const xmasDirections: [number, number][] = [
   [0, -1],
   [1, -1],
   [1, 0],
@@ -22,12 +22,12 @@ const directions: [number, number][] = [
 ];
 
 const partials = ["", "X", "XM", "XMA"];
-const check = (
+const checkXmas = (
   grid: string[][],
   curr: Point,
   [x, y]: [number, number],
   word: string,
-) => {
+): boolean => {
   // Base case
   if (word === "XMAS") {
     return true;
@@ -51,7 +51,7 @@ const check = (
   word = word + grid[curr.y][curr.x];
 
   // 2. recurse
-  if (check(grid, { x: curr.x + x, y: curr.y + y }, [x, y], word)) {
+  if (checkXmas(grid, { x: curr.x + x, y: curr.y + y }, [x, y], word)) {
     return true;
   }
 
@@ -59,24 +59,50 @@ const check = (
   return false;
 };
 
-check(grid, { x: 3, y: 9 }, [-1, -1], "");
+const checkMas = (grid: string[][], curr: Point): boolean => {
+  // can't be on the edge of the grid
+  if (
+    curr.x < 1 ||
+    curr.x >= grid[0].length - 1 ||
+    curr.y < 1 ||
+    curr.y >= grid.length - 1
+  ) {
+    return false;
+  }
 
-let count = 0;
+  if (grid[curr.y][curr.x] !== "A") {
+    return false;
+  }
+
+  const downDiag = [grid[curr.y - 1][curr.x - 1], grid[curr.y + 1][curr.x + 1]];
+  const upDiag = [grid[curr.y + 1][curr.x - 1], grid[curr.y - 1][curr.x + 1]];
+
+  return (
+    downDiag.includes("M") &&
+    downDiag.includes("S") &&
+    upDiag.includes("M") &&
+    upDiag.includes("S")
+  );
+};
+
+let count1 = 0;
+let count2 = 0;
 for (let row = 0; row < grid.length; ++row) {
   for (let col = 0; col < grid.length; ++col) {
     let curr = { x: col, y: row };
+    if (checkMas(grid, curr)) {
+      ++count2;
+    }
 
-    for (let i = 0; i < directions.length; ++i) {
-      const [x, y] = directions[i];
+    for (let i = 0; i < xmasDirections.length; ++i) {
+      const [x, y] = xmasDirections[i];
       let word = "";
-      if (check(grid, curr, [x, y], word)) {
-        if (curr.y === 9) {
-          //console.log([curr.x, curr.y], [x, y]);
-        }
-        ++count;
+      if (checkXmas(grid, curr, [x, y], word)) {
+        ++count1;
       }
     }
   }
 }
 
-console.log(count);
+console.log("part 1", count1);
+console.log("part 2", count2);
