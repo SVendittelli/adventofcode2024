b72e385945e47a3dc57c9a71d9d281926d2ae545
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-12-05 08:42:51 +0000

    feat(day05): solve part 2

--- day05/index.ts
+++ day05/index.ts
@@ -18,16 +18,21 @@ const comparator = (a, b) => {
   return mapping.lessThan.includes(b) ? -1 : 1;
 };
 
-const pages = [];
+const pages1 = [];
+const pages2 = [];
 for (let i = 0; i < linesPages.length; ++i) {
   const list = linesPages[i].split(",");
   const sorted = list.toSorted(comparator);
   const areEqual = list.every((el, id) => el === sorted[id]);
+  const middle = Math.floor(list.length / 2);
   if (areEqual) {
-    const middle = Math.floor(list.length / 2);
-    pages.push(list[middle]);
+    pages1.push(sorted[middle]);
+  } else {
+    pages2.push(sorted[middle]);
   }
 }
 
-const total1 = pages.reduce((acc, curr) => acc + +curr, 0);
-console.log(total1);
+const total1 = pages1.reduce((acc, curr) => acc + +curr, 0);
+const total2 = pages2.reduce((acc, curr) => acc + +curr, 0);
+console.log("part 1", total1);
+console.log("part 2", total2);
